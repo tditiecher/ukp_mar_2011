6a2f2b9c88436b033bef057344e5d3f3bc928bf2
Taco <taco@tdev.org>
2011-03-14 20:21:32 +0000

    Pushing new changes

--- product/nothinbutdotnetprep/utility/filtering/CriteriaExtensions.cs
+++ product/nothinbutdotnetprep/utility/filtering/CriteriaExtensions.cs
@@ -8,10 +8,21 @@ namespace nothinbutdotnetprep.utility.filtering
             return new ConditionalCriteria<ItemToMatch>(condition);
         }
 
+        public static Criteria<ItemToMatch> not<ItemToMatch>(this Criteria<ItemToMatch> criteria)
+        {
+            return new NotCriteria<ItemToMatch>(criteria);
+        }
+
         public static Criteria<ItemToMatch> or<ItemToMatch>(this Criteria<ItemToMatch> left,
-                                                            Criteria<ItemToMatch> right)
+                                                    Criteria<ItemToMatch> right)
         {
             return new OrCriteria<ItemToMatch>(left, right);
+        }
+
+        public static Criteria<ItemToMatch> and<ItemToMatch>(this Criteria<ItemToMatch> left,
+                                                            Criteria<ItemToMatch> right)
+        {
+            return new AndCriteria<ItemToMatch>(left, right);
         }
     }
 }
