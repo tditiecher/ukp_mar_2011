bb207b12c266990bf6e4f5d381777c983e2a5607
Taco <taco@tdev.org>
2011-03-14 20:19:45 +0000

    Pushing new changes

--- product/nothinbutdotnetprep/utility/filtering/OrCriteria.cs
+++ product/nothinbutdotnetprep/utility/filtering/OrCriteria.cs
@@ -18,4 +18,21 @@ namespace nothinbutdotnetprep.utility.filtering
             return left_side.matches(item) || right_side.matches(item);
         }
     }
+
+    public class AndCriteria<ItemToMatch> : Criteria<ItemToMatch>
+    {
+        Criteria<ItemToMatch> left_side;
+        Criteria<ItemToMatch> right_side;
+
+        public AndCriteria(Criteria<ItemToMatch> left_side, Criteria<ItemToMatch> right_side)
+        {
+            this.left_side = left_side;
+            this.right_side = right_side;
+        }
+
+        public bool matches(ItemToMatch item)
+        {
+            return left_side.matches(item) && right_side.matches(item);
+        }
+    }
 }
